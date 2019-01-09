06945d110d874827fe6a2f0ddf28b51056a085e9
satya_jha <satyajeetjha06@gmail.com>
2019-01-10 01:44:58 +0530

    navigation and routing

---
+++ src/pages/cartpage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default function CartPage(props){
+    return <div>
+             <h2>Mycart </h2>
+          </div>
+}
---
+++ src/pages/homepage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default function HomePage(props){
+    return <div>
+             <h2>HomePage </h2>
+          </div>
+}
